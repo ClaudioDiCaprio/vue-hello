1af01c9dd9960ec327c412b649d38e98bbae81fe
Claudio <claudiodicaprio89@gmail.com>
2021-11-25 15:46:39 +0100

    img

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+
+const app = new Vue({
+    el:'#root',
+    data :{
+        message:'Hello Vue',
+        thisImage:'img/but_it_does.png',
+
+    },
+    // methods:{
+    //     saluta:function(){
+            
+    //     }
+    // }    
+
+});
